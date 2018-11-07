8f0e56f867efc557351bd31966a6484bb9859ac4
Zheng Qian <sotux82@gmail.com>
2018-11-07 08:47:23 +0800

    ssid: use BOARD_ID replace ASUS
    Signed-off-by: Zheng Qian <sotux82@gmail.com>

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -18,6 +18,8 @@
 #ifndef _defaults_h_
 #define _defaults_h_
 
+#include <ralink_board.h>
+
 #define SYS_SHELL		"/bin/sh"
 #define SYS_EXEC_PATH		"/usr/sbin:/usr/bin:/sbin:/bin"
 #define SYS_EXEC_PATH_OPT	"/opt/sbin:/opt/bin:/usr/sbin:/usr/bin:/sbin:/bin"
@@ -35,16 +37,16 @@
 
 #define DEF_WLAN_2G_CC		"GB"
 #define DEF_WLAN_5G_CC		"GB"
-#define DEF_WLAN_2G_SSID	"ASUS"
-#define DEF_WLAN_5G_SSID	"ASUS_5G"
-#define DEF_WLAN_2G_GSSID	"ASUS_GUEST"
-#define DEF_WLAN_5G_GSSID	"ASUS_GUEST_5G"
+#define DEF_WLAN_2G_SSID	BOARD_PID
+#define DEF_WLAN_5G_SSID	BOARD_PID "_5G"
+#define DEF_WLAN_2G_GSSID	BOARD_PID "_GUEST"
+#define DEF_WLAN_5G_GSSID	BOARD_PID "_GUEST_5G"
 #define DEF_WLAN_2G_PSK		"1234567890"
 #define DEF_WLAN_5G_PSK		"1234567890"
 
 #define DEF_ROOT_PASSWORD	"admin"
 #define DEF_SMB_WORKGROUP	"WORKGROUP"
-#define DEF_TIMEZONE		"GMT0"
+#define DEF_TIMEZONE		"CST-8"
 #define DEF_NTP_SERVER0		"pool.ntp.org"
 #define DEF_NTP_SERVER1		"time.nist.gov"
 #define DEF_HTTPS_CIPH_LIST	"DH+AESGCM:DH+AES256:DH+AES:DH+3DES:RSA+AES:RSA+3DES:!ADH:!MD5:!DSS"
